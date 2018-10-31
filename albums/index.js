ddc4e1b3d8c61394e5e89345904dc5977367e58e
Craig hardy <cahardy3@googlemail.com>
2018-10-31 16:54:20 +0000

    Section 7

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,16 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
-AppRegistry.registerComponent(appName, () => App);
+//create component
+const App = () => (
+    <View>
+    <Header headerText={'Albums'} /> 
+    <AlbumList />
+    </View>
+);
+
+AppRegistry.registerComponent('albums', () => App);
