f97bd3ec791d4900f05b80b280b6fd7c16fdf38e
Craig hardy <cahardy3@googlemail.com>
2019-09-19 09:48:52 +0100

    Change
    Change

--- completeJS/3-how-JS-works/starter/script.js
+++ completeJS/3-how-JS-works/starter/script.js
@@ -1,13 +1,27 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
 
+// retirement(1956);
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
+console.log(age);
+var age = 23;
 
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
