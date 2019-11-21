c03f164a6f845dbd3a7d9113cf360f4d4482537f
Craig Hardy <cahardy3@googlemail.com>
2019-11-21 15:49:48 +0000

    Workdone

--- comapp/index.js
+++ comapp/index.js
@@ -2,17 +2,22 @@
 
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation-stack';
 import Header from './src/components/Header';
 import ComicList from './src/components/ComicList';
 import SearchBar from './src/components/SearchBar';
 
-
-const Marvel = () => (
-  <View style={{ flex:1 }}>
-      <Header headerText={'comics'} /> 
-      <SearchBar/>
-      <ComicList/>
-  </View>
+const navigator = createStackNavigator(
+  {
+    Home: SearchBar,
+    List: ComicList
+  },
+  {
+    initialRouteName: 'SearchBar',
+    defaultNavigationOptions: {
+      title: 'Marvel'
+    }
+  }
 );
 
-AppRegistry.registerComponent('comapp', () => Marvel);
+export default createAppContainer(navigator);
